b40d2b5bc2f081431d65f160d5b6bbde99708bed
Esraa Ahmed <esraa1691ahmed@gmail.com>
2025-08-15 18:58:47 +0300

    final fix

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Product, ProductService } from '../../services/product.service';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft as faArrowLeftIcon } from '@fortawesome/free-solid-svg-icons';
+
+import { ProductService, Product } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
-
-export class ProductDetailComponent implements OnInit { 
-  faArrowLeft = faArrowLeft;
+export class ProductDetailComponent implements OnInit {
+  faArrowLeft: IconProp = faArrowLeftIcon;
   product?: Product;
 
-  constructor(private route: ActivatedRoute, private productSvc: ProductService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private productSvc: ProductService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('productId'));
     this.product = this.productSvc.getProductById(id);
   }
 }
+
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { Product, ProductService } from '../../services/product.service';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCircleRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+// alias imports to avoid name shadowing with fields
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCircleRight as faCircleRightIcon,
+  faArrowLeft as faArrowLeftIcon,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { ProductService, Product } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-
 export class ProductListComponent implements OnInit {
-  faCircleRight = faCircleRight;
-  faArrowLeft   = faArrowLeft; 
+  faCircleRight: IconProp = faCircleRightIcon;
+  faArrowLeft:  IconProp = faArrowLeftIcon;
+
   categoryName = '';
   products: Product[] = [];
 
@@ -26,11 +33,13 @@ export class ProductListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.categoryName = this.route.snapshot.paramMap.get('categoryName') || '';
-    this.products = this.productSvc.getProductsByCategory(this.categoryName);
+    this.route.paramMap.subscribe(pm => {
+      this.categoryName = pm.get('categoryName') ?? '';
+      this.products = this.productSvc.getProductsByCategory(this.categoryName);
+    });
   }
 
   goToDetails(p: Product) {
-    this.router.navigate(['/category', this.categoryName, 'product', p.id]);
+    this.router.navigate(['/category', p.category, 'product', p.id]);
   }
 }
